498c0403c499105e24db4901f79aa41db74d476f
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-07-27 12:28:25 -0500

    Remove extra css

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<h1 class="mv0 f2 lh-title"><?php the_title(); ?></h1>
+<h1><?php the_title(); ?></h1>
 
 <?php
   if ( have_posts() ) : while ( have_posts() ) : the_post();
