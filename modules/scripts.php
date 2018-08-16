5f6ddc43ef9a4ee85f76cfb211171335d7fd37d6
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-08-16 11:17:07 -0500

    Fix stylesheet and script link version tracking

--- modules/scripts.php
+++ modules/scripts.php
@@ -3,7 +3,11 @@
 // Load scripts into theme
 
 function theme_scripts() {
-	wp_enqueue_script( 'main', get_template_directory_uri() . '/scripts/main.js', array( 'jquery' ), null, true);
+	// Gets the theme version and automatically appends it to the stylesheet url
+	$theme = wp_get_theme();
+	$version = $theme->get('Version');
+
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/scripts/main.js', array( 'jquery' ), $version, true);
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
--- modules/styles.php
+++ modules/styles.php
@@ -8,11 +8,10 @@ $font_url = null;
 // If you want to use Tachyons flip this to true! Go get the newest version if you want and paste it below in the Tachyons wp_enqueue_style.
 $tachyons = false;
 
-// Gets the theme version and automatically appends it to the stylesheet url
-$theme = wp_get_theme();
-$version = $theme->get('Version');
-
 function theme_styles() {
+  // Gets the theme version and automatically appends it to the stylesheet url
+  $theme = wp_get_theme();
+  $version = $theme->get('Version');
 
   if ($tachyons) {
     wp_enqueue_style( 'tachyons', 'https://unpkg.com/tachyons@4.9.0/css/tachyons.min.css');
